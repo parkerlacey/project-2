38554ce147a7f23a65a8ef063d1297bacdca0b2a
EricaE <106550558+ericaemorabito@users.noreply.github.com>
2022-11-04 08:37:40 -0500

    working on server/db connection

---
+++ server.js
@@ -0,0 +1,35 @@
+// Import expression and sessions
+const express = require('express');
+const session = require('express-session');
+
+// Import routes from controllers folder
+const routes = require('./controllers/index');
+
+// Import connection to database
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+// Create session data?
+const sess = {
+  secret: 'Super secret secret',
+  cookie: {},
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
+
+app.use(session(sess));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.use(routes);
+
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log('Now listening'));
+});
