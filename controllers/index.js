27c74228a07ea52c71b6bb27b9b19286d1bb2f44
EricaE <106550558+ericaemorabito@users.noreply.github.com>
2022-11-07 16:07:49 -0600

    connect to db/server

--- controllers/index.js
+++ controllers/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 
-// Testing server routes with get request
+// Testing server is working with get request
 router.get('/', async (req, res) => {
   try {
     res.send('Testing to see if we can hit this route.');
